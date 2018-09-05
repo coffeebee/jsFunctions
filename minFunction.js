40b80a83ba6e0ecf19cfa1f95b75031b3fcfc441
coffeebee <dabbss@otc.edu>
2018-09-05 10:18:41 -0500

    YAY

--- minFunction.js
+++ minFunction.js
@@ -1,11 +1,16 @@
 const min = (x,y) =>{
-    if(x<y)
+    if((typeof x ==='string') || (typeof y ==='string')){
+        return "NA";
+    }
+    else if(x<y)
         return x;    
     else if(y<x)
         return y;
-    else 
-        return x;
+    else (x===y)
+        return x;   
 }
 console.log(min(4.5,5));
 console.log(min(19,9));
 console.log(min(1,1));
+
+//console.log(min("dog", "to"));
